1a74b7bef850a6f42266a1bde5f7a0773dd6a784
Rıfat Duru <rifatduru@Rfats-MacBook-Air.local>
2019-01-26 15:10:39 +0300

    Dropdown icerisine list verileri getirildi.

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(),
+      home: CitySelectorWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class CitySelectorWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CitySelectorState();
+}
+
+class CitySelectorState extends State<CitySelectorWidget> {
+  var cities = ["Ankara", "İstanbul", "Antalya", "Eskişehir", "Trabzon"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("City Selection App"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            DropdownButton<String>(
+              items: cities.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              value: "İstanbul",
+              onChanged: (String value) {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/* class MyHomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => StudentDialogState();  
-  }
-  
-class StudentDialogState extends State<MyHomePage>{
+  } */
+
+/* class StudentDialogState extends State<MyHomePage>{
   String studentName = "";
   @override
   Widget build(BuildContext context) {
@@ -39,10 +71,9 @@ class StudentDialogState extends State<MyHomePage>{
               });
             },
           ),
-          Text("Hello :"+studentName)
+          Text( "Hello :"+studentName)
         ],
       ),
     );
   }
-
-}
+} */
